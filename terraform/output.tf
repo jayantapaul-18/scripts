3c548e90cac90899fb2b894d00b20ee6f3fb5603
Jayanta Paul <jayantapaul.jp18@gmail.com>
2024-03-08 12:30:44 -0800

    Create output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,19 @@
+Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
+
+Outputs:
+
+project_info = {
+  maintainers = [
+    {
+      access_level = 50
+      avatar_url   = "https://gitlab.com/uploads/-/system/user/avatar/123456/avatar.png"
+      email        = "maintainer@example.com"
+      id           = 123456
+      name         = "Maintainer Name"
+      username     = "maintainer_username"
+    },
+    # Additional maintainers...
+  ]
+  project_id   = 123456
+  project_name = "Your Project Name"
+}
